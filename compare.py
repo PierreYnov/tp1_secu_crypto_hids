2829e54852f96b9433dbca1bc9d591267b3316e1
Durand <emma.durand@ynov.com>
2020-11-24 13:44:55 +0100

    change script

--- compare.py
+++ compare.py
@@ -1,24 +1,46 @@
 import hashlib
 
-question = input("Entrez le nom du fichier (avec sa direction si il n'est pas la) : ")
+#question = input("Entrez le nom du fichier (avec sa direction si il n'est pas la) : ")
 
 # Fichier BDD
-hasher1 = hashlib.md5()
-afile1 = open(question, 'rb')
+path_conf = "/etc/aide/aide.conf"
+path_db = "/var/lib/aide/aide.db"
+
+hasher2 = hashlib.md5()
+
+# Conf aide
+afile1 = open(path_conf, 'rb')
 buf1 = afile1.read()
-a = hasher1.update(buf1)
-md5_a = (str(hasher1.hexdigest()))
-hash_fichier = str(md5_a)
-print(hash_fichier)
+a = hasher2.update(buf1)
+md5_a = (str(hasher2.hexdigest()))
+hash_fichier_conf = str(md5_a)
+
+
+# Aide bdd
+afile2 = open(path_db, 'rb')
+buf2 = afile2.read()
+b = hasher2.update(buf2)
+md5_b = (str(hasher2.hexdigest()))
+hash_fichier_db = str(md5_b)
+
+
+print("HASH 'aide.conf' : " + hash_fichier_conf)
+print("HASH 'aide.db' : " + hash_fichier_db)
 
 # Lecture du Hash de l'ancienne BDD
-f = open("myfile.txt", "r")
+f = open("aide_conf.txt", "r")
+f2 = open("db_conf.txt", "r")
 
-hash_bdd_orig = f.read()
-print(hash_bdd_orig)
+
+hash_conf_orig = f.read()
+hash_bdd_orig = f2.read()
+
+print("")
+print("HASH ORIGINE 'aide.conf' : " + hash_conf_orig)
+print("HASH ORIGINE 'aide.db' : " + hash_bdd_orig)
 
 # Compare md5
-if hash_fichier == hash_bdd_orig:
-    print("C'est le même hash, vous pouvez lancer le fichier")
+if (hash_fichier_conf == hash_conf_orig) and (hash_fichier_db == hash_bdd_orig) :
+    print("Ce sont les mêmes HASH, vous pouvez lancer le fichier")
 else:
-    print("Non arrêtez tout !")
+    print("STOP, fichier corrompu !")
--- hash.py
+++ hash.py
@@ -1,14 +1,28 @@
 import hashlib
 
-question = input("Entrez le nom du fichier (avec sa direction si il n'est pas la) : ")
+path_conf = "/etc/aide/aide.conf"
+path_bdd = "/var/lib/aide/aide.db"
 
 hasher2 = hashlib.md5()
-afile2 = open(question, 'rb')
-buf2 = afile2.read()
+
+# Hash aide.conf
+file_conf = open(path_conf, 'rb')
+buf2 = file_conf.read()
 b = hasher2.update(buf2)
 md5_b = (str(hasher2.hexdigest()))
+# Hash in txt
+aide_txt = open('aide_conf.txt', 'w')
+aide_txt.writelines(md5_b)
+aide_txt.close()
 
-file1 = open('myfile.txt', 'w')
-file1.writelines(md5_b)
-file1.close()
+# Hash aide.db
+file_db = open(path_bdd, 'rb')
+buf = file_db.read()
+b2 = hasher2.update(buf)
+md5 = (str(hasher2.hexdigest()))
+db_txt = open('db_conf.txt', 'w')
+db_txt.writelines(md5)
+db_txt.close()
 
+print("HASH 'aide.conf': " + md5_b)
+print("HASH 'aide.db': " + md5)
